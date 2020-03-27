b5f235d18306669a20a420b12a0498a888276a69
Richard Cespedes Lopez <riceslo@hotmail.com>
2020-03-26 21:27:25 -0400

    Create Authirozation

---
+++ Infrastructure/Commands/V1/Invoice/CreateInvoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Commands.V1.Invoice
+{
+    class CreateInvoice
+    {
+
+        public int InvoiceNumber { get; set; }
+        public String ClientName { get; set; }
+        public String TaxPayerIdentificationNumber { get; set; }
+        public DateTime EmisionDate { get; set; }
+        public String AuthorizationId { get; set; }
+        
+    }
+}
